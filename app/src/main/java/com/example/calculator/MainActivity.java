8692a084ae59fc89c8a3612ed125c8f376fc9ad3
Olga Papkovskaya <o.papkovskaya@gmail.com>
2017-10-24 01:16:33 +0300

    add floats
    add floats

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -1,13 +1,147 @@
 package com.example.calculator;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.app.Activity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.calculator.polish_processing.PolishProcessor;
+
+import java.util.LinkedList;
+
+
+public class MainActivity extends Activity implements OnClickListener {
+    EditText str;
+
+    int operand, ost, flagAction, len, pow;
+    double result;
+    float floatOst, floatOperand;
+    String string;
+    LinkedList<Float> st = new LinkedList<>();
+    LinkedList<Character> op = new LinkedList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        str = findViewById(R.id.viewNumbers);
+        operand = 0;
+        result = 0;
+        flagAction = 0;
+        floatOperand = 0;
+        pow = 1;
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch(v.getId()){
+            case R.id.one:
+                ClickNumber(1);
+                break;
+
+            case R.id.two:
+                ClickNumber(2);
+                break;
+
+            case R.id.three:
+                ClickNumber(3);
+                break;
+
+            case R.id.four:
+                ClickNumber(4);
+                break;
+
+            case R.id.five:
+                ClickNumber(5);
+                break;
+
+            case R.id.six:
+                ClickNumber(6);
+                break;
+
+            case R.id.seven:
+                ClickNumber(7);
+                break;
+
+            case R.id.eight:
+                ClickNumber(8);
+                break;
+
+            case R.id.nine:
+                ClickNumber(9);
+                break;
+
+            case R.id.zero:
+                ClickNumber(0);
+                break;
+
+            case R.id.plus:
+                ClickOperator(R.string.plus);
+                break;
+
+            case R.id.minus:
+                ClickOperator(R.string.minus);
+                break;
+
+            case R.id.multiply:
+                ClickOperator(R.string.multiply);
+                break;
+
+            case R.id.divide:
+                ClickOperator(R.string.divide);
+                break;
+
+            case R.id.dot:
+                ClickDoubleNumber();
+                break;
+
+            case R.id.equal:
+                string = str.getText().toString();
+                result = PolishProcessor.eval(string);
+                str.setText(Double.toString(result));
+                operand = 0;
+                floatOperand = 0;
+                result = 0;
+                flagAction = 0;
+                break;
+
+            case R.id.clear:
+                operand = 0;
+                floatOperand = 0;
+                result = 0;
+                flagAction = 0;
+                str.setText(Integer.toString(operand));
+                break;
+        }
+
+    }
+
+    private void ClickDoubleNumber() {
+        str.append(".");
+    }
+
+
+    private void ClickNumber(int num){
+        if(flagAction == 0){
+            operand = operand*10 + num;
+            str.setText(Integer.toString(num));
+            flagAction = 1;
+        }else if (flagAction == 1){
+            operand = operand*10 + num;
+            str.append(Integer.toString(num));
+        }
+    }
+
+    private void ClickOperator(int stringResourceId){
+        str.append(this.getResources().getString(stringResourceId));
+        operand = 0;
+        floatOperand = 0;
+        ost = 0;
+        floatOst = 0;
     }
 }
---
+++ app/src/main/java/com/example/calculator/polish_processing/PolishProcessor.java
@@ -0,0 +1,79 @@
+package com.example.calculator.polish_processing;
+
+import java.util.LinkedList;
+
+/**
+ * Created by Ольга on 20.10.17.
+ */
+
+public class PolishProcessor {
+    static boolean isDelim(char c) {
+        return c == ' ';
+    }
+    static boolean isOperator(char c) {
+        return c == '+' || c == '-' || c == '*' || c == '/' || c == '%';
+    }
+    static int priority(char op) {
+        switch (op) {
+            case '+':
+            case '-':
+                return 1;
+            case '*':
+            case '/':
+            case '%':
+                return 2;
+            default:
+                return -1;
+        }
+    }
+    static void processOperator(LinkedList<Float> st, char op) {
+        float r = st.removeLast();
+        float l = st.removeLast();
+        switch (op) {
+            case '+':
+                st.add(l + r);
+                break;
+            case '-':
+                st.add(l - r);
+                break;
+            case '*':
+                st.add(l * r);
+                break;
+            case '/':
+                st.add(l / r);
+                break;
+            case '%':
+                st.add(l % r);
+                break;
+        }
+    }
+    public static Float eval(String s) {
+        LinkedList<Float> st = new LinkedList<>();
+        LinkedList<Character> op = new LinkedList<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (isDelim(c))
+                continue;
+            if (c == '(')
+                op.add('(');
+            else if (c == ')') {
+                while (op.getLast() != '(')
+                    processOperator(st,op.removeLast());
+                op.removeLast();
+            } else if (isOperator(c)) {
+                while (!op.isEmpty() && priority(op.getLast()) >= priority(c))
+                    processOperator(st, op.removeLast());
+                op.add(c);
+            } else {
+                String operand = "";
+                while (i < s.length() && Character.isDigit(s.charAt(i)) || i < s.length() && s.charAt(i) == '.' )
+                    operand += s.charAt(i++);
+                --i;
+                st.add(Float.parseFloat(operand));
+            }
+        }
+        while (!op.isEmpty())
+            processOperator(st, op.removeLast());
+        return st.get(0);
+    }
+}
